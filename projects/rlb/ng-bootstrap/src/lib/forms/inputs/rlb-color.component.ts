067ac66724666d7c8cc7cf5960d9fc5e6b2fa94f
Giuseppe Riolo <riolo.giuseppe@gmail.com>
2023-07-23 22:31:25 +0200

    added color

---
+++ projects/rlb/ng-bootstrap/src/lib/forms/inputs/rlb-color.component.ts
@@ -0,0 +1,36 @@
+import { ChangeDetectionStrategy, Component, Input, Optional, Self } from '@angular/core';
+import { ControlValueAccessor, NgControl } from '@angular/forms';
+import { RlbAbstractComponent } from './rlb-abstract-field.component';
+import { UniqueIdService } from "../../shared/unique-id.service";
+
+@Component({
+  selector: 'rlb-color',
+  template: `
+  <label [for]="id" class="form-label">{{label}}</label>
+  <input #input
+         [id]="id"
+         class="form-control form-control-color" 
+         type="color" 
+         [attr.disabled]="disabled?true:undefined" 
+         [attr.readonly]="readonly?true:undefined"              
+         [value]="value"
+         (blur)="touch();"             
+         [ngClass]="{'is-invalid': control?.touched && control?.invalid}"
+         (input)="update($event.target);">`
+})
+export class RlbColorComponent extends RlbAbstractComponent<string> implements ControlValueAccessor {
+  @Input() disabled = false;
+  @Input() readonly = false;
+  @Input() label: string = '';
+
+  constructor(idService: UniqueIdService, @Self() @Optional() override control?: NgControl) {
+    super(idService, control)
+  }
+
+  update(ev: (EventTarget | null)) {
+    if (!this.disabled) {
+      const t = (ev as HTMLInputElement)
+      this.setValue(t?.value)
+    }
+  }
+}
