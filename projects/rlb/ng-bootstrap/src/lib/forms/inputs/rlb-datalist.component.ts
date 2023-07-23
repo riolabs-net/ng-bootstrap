e6258bb5b7c56c92338b7d47486abe7043214b3a
Giuseppe Riolo <riolo.giuseppe@gmail.com>
2023-07-23 23:52:18 +0200

    added datalist

---
+++ projects/rlb/ng-bootstrap/src/lib/forms/inputs/rlb-datalist.component.ts
@@ -0,0 +1,44 @@
+import { ChangeDetectionStrategy, Component, Input, Optional, Self } from '@angular/core';
+import { ControlValueAccessor, NgControl } from '@angular/forms';
+import { RlbAbstractComponent } from './rlb-abstract-field.component';
+import { UniqueIdService } from "../../shared/unique-id.service";
+
+@Component({
+  selector: 'rlb-datalist',
+  template: `
+  <label [for]="id" class="form-label">{{label}}</label>
+  <input #input
+         [id]="id"
+         class="form-control" 
+         [attr.disabled]="disabled?true:undefined"
+         [attr.readonly]="readonly?true:undefined"
+         [attr.placeholder]="placeholder"
+         [attr.list]="'list-'+id"
+         [class.form-control-lg]="size === 'large'"
+         [class.form-control-sm]="size === 'small'"
+         [value]="value"
+         (blur)="touch();"
+         [ngClass]="{'is-invalid': control?.touched && control?.invalid}"
+         (input)="update($event.target);">
+  <datalist [id]="'list-'+id">
+    <ng-content></ng-content>
+  </datalist>`
+})
+export class RlbDatalistComponent extends RlbAbstractComponent<string> implements ControlValueAccessor {
+  @Input() disabled = false;
+  @Input() readonly = false;
+  @Input() label: string = '';
+  @Input() placeholder!: string
+  @Input() size: "small" | "large" | undefined = undefined
+
+  constructor(idService: UniqueIdService, @Self() @Optional() override control?: NgControl) {
+    super(idService, control)
+  }
+
+  update(ev: (EventTarget | null)) {
+    if (!this.disabled) {
+      const t = (ev as HTMLInputElement)
+      this.setValue(t?.value)
+    }
+  }
+}
