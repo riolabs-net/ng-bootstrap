5bb770946208fd18e3c0d2775e9dcc472edbb9f7
Giuseppe Riolo <riolo.giuseppe@gmail.com>
2023-07-23 22:45:13 +0200

    added range control

---
+++ projects/rlb/ng-bootstrap/src/lib/forms/inputs/rlb-range.component.ts
@@ -0,0 +1,43 @@
+import { ChangeDetectionStrategy, Component, Input, Optional, Self } from '@angular/core';
+import { ControlValueAccessor, NgControl } from '@angular/forms';
+import { RlbAbstractComponent } from './rlb-abstract-field.component';
+import { UniqueIdService } from "../../shared/unique-id.service";
+
+@Component({
+  selector: 'rlb-range',
+  template: `
+  <label [for]="id" class="form-label">{{ label }}</label>
+  <input #input
+         [id]="id"
+         class="form-range" 
+         type="range" 
+         [attr.disabled]="disabled?true:undefined"
+         [attr.readonly]="readonly?true:undefined"
+         [attr.min]="min"
+         [attr.max]="max"
+         [attr.step]="step"
+         [value]="value"
+         (blur)="touch();"
+         [ngClass]="{'is-invalid': control?.touched && control?.invalid}"
+         (input)="update($event.target);"
+         >`
+})
+export class RlbRangeComponent extends RlbAbstractComponent<string> implements ControlValueAccessor {
+  @Input() disabled = false;
+  @Input() readonly = false;
+  @Input() label: string = '';
+  @Input() min?: number | undefined = undefined
+  @Input() max?: number | undefined = undefined
+  @Input() step?: number | undefined = undefined
+
+  constructor(idService: UniqueIdService, @Self() @Optional() override control?: NgControl) {
+    super(idService, control)
+  }
+
+  update(ev: (EventTarget | null)) {
+    if (!this.disabled) {
+      const t = (ev as HTMLInputElement)
+      this.setValue(t?.value)
+    }
+  }
+}
