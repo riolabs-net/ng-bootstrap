0f8a89be094a16022eeb8d84ef7e5ce803933ade
Giuseppe Riolo <riolo.giuseppe@gmail.com>
2023-07-25 17:19:51 +0200

    alert component

--- projects/rlb/ng-bootstrap/src/lib/components/alert.component.ts
+++ projects/rlb/ng-bootstrap/src/lib/components/alert.component.ts
@@ -1,9 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Color } from '../shared/colors';
 
 @Component({
   selector: 'rlb-alert',
-  template: ``
+  template: `
+    <div class="alert alert-{{color}}" role="alert">
+      <ng-content></ng-content>
+      <button *ngIf="dismissible" type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close" (click)="dismissed.emit()"></button>
+    </div>`
 })
 export class AlertComponent {
-
+  @Input() color: Color = 'primary';
+  @Input() dismissible = false;
+  @Output() dismissed: EventEmitter<void> = new EventEmitter<void>();
 }
